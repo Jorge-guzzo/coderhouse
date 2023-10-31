68c1d1a6009e2038bfc4f37873016150d356048f
Jorge-guzzo <119744221+Jorge-guzzo@users.noreply.github.com>
2023-10-31 16:55:56 -0300

    Add files via upload

---
+++ config = configParser().py
@@ -0,0 +1,9 @@
+from configparser import ConfigParser
+
+config = ConfigParser()
+
+config_dir = "c:/Users/Jorge/OneDrive/METAS/CODERHOUSE/config/config = configParser().py"
+config.read(config_dir)
+['config/config.ini']
+
+config.sections()
