906d92fc7ea46d4b35a3c1e0a4653e1090a101c2
Jorge-guzzo <119744221+Jorge-guzzo@users.noreply.github.com>
2023-11-02 17:12:17 -0300

    Delete config = configParser().py

--- config = configParser().py
+++
@@ -1,9 +0,0 @@
-from configparser import ConfigParser
-
-config = ConfigParser()
-
-config_dir = "c:/Users/Jorge/OneDrive/METAS/CODERHOUSE/config/config = configParser().py"
-config.read(config_dir)
-['config/config.ini']
-
-config.sections()
